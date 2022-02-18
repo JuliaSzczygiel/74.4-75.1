8143352d3ecccf2f01825cac75e67fd262ccfeac
JuliaSzczygiel <62343530+JuliaSzczygiel@users.noreply.github.com>
2022-02-18 09:39:46 +0100

    Add files via upload

---
+++ zad75.1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+	
+cout << "test: " << 'c' - 'a' << endl;
+	
+	ifstream dane("tekst.txt");
+	string slowo;
+	vector<string> podpunkt1;
+	while(dane >> slowo){
+		if(slowo[0] == 'd' && slowo[slowo.length()-1] == 'd'){
+			podpunkt1.push_back(slowo);
+		}
+	}
+	
+	cout << "75.1: " << endl;
+	for(int a=0; a<podpunkt1.size(); a++){
+		cout << podpunkt1[a] << endl;
+	}
+}

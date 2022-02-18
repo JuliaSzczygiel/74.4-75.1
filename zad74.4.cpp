caa3c12b6300e763f1c108a7718e50c3847739ad
JuliaSzczygiel <62343530+JuliaSzczygiel@users.noreply.github.com>
2022-02-18 09:44:44 +0100

    Update zad74.4.cpp

--- zad74.4.cpp
+++ zad74.4.cpp
@@ -4,19 +4,28 @@ using namespace std;
 
 int main(){
 	
-cout << "test: " << 'c' - 'a' << endl;
-	
-	ifstream dane("tekst.txt");
-	string slowo;
-	vector<string> podpunkt1;
-	while(dane >> slowo){
-		if(slowo[0] == 'd' && slowo[slowo.length()-1] == 'd'){
-			podpunkt1.push_back(slowo);
+ifstream dane("hasla.txt");
+	string haslo;
+	int mocnychHasel = 0;
+	while(dane >> haslo){
+		bool znakNumeryczyny = false;
+		bool malaLitera = false;
+		bool duzaLitera = false;
+		for(int a=0; a<haslo.size(); a++){
+			if((int)haslo[a] >= (int)'0' && (int)haslo[a] <= (int)'9'){
+				znakNumeryczyny = true;
+			}
+			if((int)haslo[a] >= (int)'a' && (int)haslo[a] <= (int)'z'){
+				malaLitera = true;
+			}
+			if((int)haslo[a] >= (int)'A' && (int)haslo[a] <= (int)'Z'){
+				duzaLitera = true;
+			}
+		}
+		if(znakNumeryczyny && malaLitera && duzaLitera){
+			mocnychHasel++;
 		}
 	}
-	
-	cout << "75.1: " << endl;
-	for(int a=0; a<podpunkt1.size(); a++){
-		cout << podpunkt1[a] << endl;
-	}
+	cout << "74.4: " << endl;
+	cout << "Ilosc mocnych hasel: " << mocnychHasel << endl;
 }
